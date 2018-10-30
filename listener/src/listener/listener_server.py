c721bc1b299ffdc0bf60df41359bb7f56e2c0701
Nathan Thom <nathanthom@nevada.unr.edu>
2018-10-30 08:08:09 -0700

    First attempt at solving the problem of not being able to access DeepSpeech/models on different machines. Also changed the compression of DeepSpeech from .tar.gz to .zip.

--- listener/src/listener/listener_server.py
+++ listener/src/listener/listener_server.py
@@ -14,15 +14,20 @@ def handle_listener(req):
     # Ref: https://progur.com/2018/02/how-to-use-mozilla-deepspeech-tutorial.html
     #      https://github.com/mozilla/DeepSpeech/blob/master/native_client/python/client.py
 
-    ALPHABET = os.getcwd() + "/DeepSpeech/models/alphabet.txt"
-    OUTPUT_GRAPH = os.getcwd() + "/DeepSpeech/models/output_graph.pb"
-    audio_file = glob.glob(os.getcwd() + "/DeepSpeech/audio/" + req.filename)
+    file = sys.argv[0]
+    path = os.path.dirname(file)
+    print("File: ", file)
+    print("Path: ", path)
+
+    ALPHABET = path + "/DeepSpeech/models/alphabet.txt"
+    OUTPUT_GRAPH = path + "/DeepSpeech/models/output_graph.pb"
+    audio_file = glob.glob(path + "/DeepSpeech/audio/" + req.filename)
 
     model = Model(OUTPUT_GRAPH, 26, 9, ALPHABET, 500)
-    print("audio_file", audio_file[0])
     fs, audio = wav.read(audio_file[0])
     data = model.stt(audio, fs)
 
+    print "audio file ", audio_file[0], " served to client. \n"
     print "Ready to interpret more speech audio."
 
     return data
