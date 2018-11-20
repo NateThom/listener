309719e4e3927c1c8a71423aa6bfabdec56f28d3
Bryson Lingenfelter <blingenfelter@nevada.unr.edu>
2018-11-20 14:11:03 -0800

    allow different rates / devices

--- src/unr_deepspeech_client.py
+++ src/unr_deepspeech_client.py
@@ -10,14 +10,36 @@ import wave
 
 import rospy
 from unr_deepspeech.srv import *
+from talker.srv import *
 
 from rospkg import RosPack
-import webrtcvad
+import audioop
 
 THRESHOLD = 500
 CHUNK_SIZE = 1024
 FORMAT = pyaudio.paInt16
-RATE = 16000
+
+# See: en.wikipedia.org/wiki/Levenshtein_distance
+def levenshtein_distance(str1, str2):
+    if len(str2) > len(str1):
+        str2, str1 = str1, str2
+    row_count = len(str1)
+    col_count = len(str2)
+
+    row_current = range(row_count + 1)
+    for row_n in xrange(row_count):
+        row_next = [row_n + 1]
+        for col_n in xrange(col_count):
+            delete_cost = row_current[col_n + 1] + 1
+            insert_cost = row_next[col_n] + 1
+            substitution_cost = row_current[col_n] + 1
+            if str1[row_n] == str2[col_n]:
+                substitution_cost = row_current[col_n]
+
+            row_next.append(min([substitution_cost, delete_cost,
+                                 insert_cost]))
+        row_current = row_next
+    return row_current[-1]
 
 def unr_deepspeech_client(filename):
     rospy.wait_for_service('listen')
@@ -66,27 +88,27 @@ def trim(snd_data):
     snd_data.reverse()
     return snd_data
 
-def add_silence(snd_data, seconds):
+def add_silence(snd_data, seconds, rate):
     "Add silence to the start and end of 'snd_data' of length 'seconds' (float)"
-    r = array('h', [0 for i in xrange(int(seconds*RATE))])
+    r = array('h', [0 for i in xrange(int(seconds*rate))])
     r.extend(snd_data)
-    r.extend([0 for i in xrange(int(seconds*RATE))])
+    r.extend([0 for i in xrange(int(seconds*rate))])
     return r
 
-def record():
+def record(rate, device):
     """
-    Record a word or words from the microphone and 
+    Record a word or words from the microphone and
     return the data as an array of signed shorts.
 
-    Normalizes the audio, trims silence from the 
-    start and end, and pads with 0.5 seconds of 
-    blank sound to make sure VLC et al can play 
+    Normalizes the audio, trims silence from the
+    start and end, and pads with 0.5 seconds of
+    blank sound to make sure VLC et al can play
     it without getting chopped off.
     """
     p = pyaudio.PyAudio()
-    stream = p.open(format=FORMAT, channels=1, rate=RATE,
-        input=True, output=True,
-        frames_per_buffer=CHUNK_SIZE)
+    stream = p.open(format=FORMAT, channels=1, rate=rate,
+        input=True, output=True, frames_per_buffer=CHUNK_SIZE,
+        input_device_index=device)
 
     num_silent = 0
     snd_started = False
@@ -95,7 +117,7 @@ def record():
 
     while 1:
         # little endian, signed short
-        snd_data = array('h', stream.read(CHUNK_SIZE))
+        snd_data = array('h', stream.read(CHUNK_SIZE, exception_on_overflow=False))
         if byteorder == 'big':
             snd_data.byteswap()
         r.extend(snd_data)
@@ -117,27 +139,44 @@ def record():
 
     r = normalize(r)
     r = trim(r)
-    r = add_silence(r, 0.5)
+    r = add_silence(r, 0.5, rate)
     return sample_width, r
 
-def record_to_file(path):
+def record_to_file(path, rate, device):
     "Records from the microphone and outputs the resulting data to 'path'"
-    sample_width, data = record()
-    data = pack('<' + ('h'*len(data)), *data)
+    sample_width, data = record(rate, device)
+
+    if rate != 16000:
+        data_16GHz = audioop.ratecv(data, sample_width, 1, 48000, 16000, None)[0]
+    else:
+        data_16GHz = pack('<' + ('h'*len(data)), *data)
 
     wf = wave.open(path, 'wb')
     wf.setnchannels(1)
     wf.setsampwidth(sample_width)
-    wf.setframerate(RATE)
-    wf.writeframes(data)
+    wf.setframerate(16000)
+    wf.writeframes(data_16GHz)
     wf.close()
 
 if __name__ == "__main__":
+    device = 6
+    rate = 48000
+    print("Current device is {}. Please make sure this is the device you want".format(device))
+    print("Current sample rate is {}".format(rate))
+    # Print audio devices
+    # Ref: https://stackoverflow.com/a/39677871
+    p = pyaudio.PyAudio()
+    info = p.get_host_api_info_by_index(0)
+    numdevices = info.get('deviceCount')
+    for i in range(0, numdevices):
+         if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+             print "Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name')
+
     rospy.init_node("unr_deepspeech_client")
     current_time = int(rospy.get_time())
     rp = RosPack()
     audio_path = rp.get_path("unr_deepspeech") + "/data"
-    record_to_file("{}/{}.wav".format(audio_path, current_time))
+    record_to_file("{}/{}.wav".format(audio_path, current_time), rate=rate, device=device)
 
     print("Text: {}".format(unr_deepspeech_client("{}/{}.wav".format(audio_path, current_time))))
 
@@ -145,4 +184,3 @@ if __name__ == "__main__":
     keep_wav = rospy.get_param("/unr_deepspeech/keep_wav", False)
     if not keep_wav:
         os.remove("{}/{}.wav".format(audio_path, current_time))
-    
